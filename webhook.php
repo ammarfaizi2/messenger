ab5a4519a4663345134e58314bebb4615dd13cab
Ammar Faizi <ammarfaizi2@gmail.com>
2017-05-27 15:20:20 +0700

    Update webhook

--- webhook.php
+++ webhook.php
@@ -3,15 +3,13 @@ require __DIR__ . '/config.php';
 require __DIR__ . '/vendor/autoload.php';
 use System\Messsenger;
 
-
+header('Content-type:application/json');
 
 if (isset($_GET['pg']) && isset($config[$_GET['pg']])) {
-	$bot = new Messsenger($config[$_GET['pg']]['validation'], $config[$_GET['token']]);
-
-
+	$bot = new Messsenger($config[$_GET['pg']]['validation'], $config[$_GET['pg']]['token']);
 	switch ($_GET['action']) {
 		case 'set_welcome':
-			$updated = $bot->setWelcomeMessage(FACEBOOK_PAGE_ID, "Greetings! The humans who invented me programmed me to tell you about...");
+			$updated = $bot->setWelcomeMessage($config[$_GET['pg']]['page_id'], $config[$_GET['pg']]['welcome_msg']);
 			if($updated){
 				echo "Welcome Message updated succesfully!";
 			} else {
@@ -23,4 +21,7 @@ if (isset($_GET['pg']) && isset($config[$_GET['pg']])) {
 			# code...
 			break;
 	}
+	die;
 }
+http_response_code(404);
+die;
