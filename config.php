ac9c805fb604c409d0712dbf47227b50679ecde7
Ammar Faizi <ammarfaizi2@gmail.com>
2017-05-27 15:18:10 +0700

    Update config

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+$config['esteh'] = array(
+		'token'=>'EAAPFA1dRfC4BACBL5nQgiZAwcB08Y07pOS2tPdrL7yAt6vMhEpNda7yhk7AuuZCecoROYUWNQcjIzGjYYt24TpfTlclooCV5uTwDCmALfb9GSv2ZBWn55rYZClGbZAgZANVcj6drZAvE1AjzliwTxGsaXOpAl0qQXaeZA4eNRme83QZDZD',
+		'validation'=>'858869123',
+		'page_id'=>'926837650793495'
+	);
--- webhook.php
+++ webhook.php
@@ -3,12 +3,24 @@ require __DIR__ . '/config.php';
 require __DIR__ . '/vendor/autoload.php';
 use System\Messsenger;
 
-$bot = new Messsenger(FACEBOOK_VALIDATION_TOKEN, FACEBOOK_PAGE_ACCESS_TOKEN);
 
-$updated = $bot->setWelcomeMessage(FACEBOOK_PAGE_ID, "Greetings! The humans who invented me programmed me to tell you about...");
 
-if($updated){
-	echo "Welcome Message updated succesfully!";
-} else {
-	echo "Error during Welcome Message update";
+if (isset($_GET['pg']) && isset($config[$_GET['pg']])) {
+	$bot = new Messsenger($config[$_GET['pg']]['validation'], $config[$_GET['token']]);
+
+
+	switch ($_GET['action']) {
+		case 'set_welcome':
+			$updated = $bot->setWelcomeMessage(FACEBOOK_PAGE_ID, "Greetings! The humans who invented me programmed me to tell you about...");
+			if($updated){
+				echo "Welcome Message updated succesfully!";
+			} else {
+				echo "Error during Welcome Message update";
+			}
+			break;
+		
+		default:
+			# code...
+			break;
+	}
 }
